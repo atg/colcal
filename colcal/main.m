dae70a730a153d86bb3755272753522e061a92ed
Alex Gordon <anything@fileability.net>
2012-05-25 20:51:15 +0100

    Initial commit

--- colcal/main.m
+++ colcal/main.m
@@ -6,16 +6,19 @@
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <Cocoa/Cocoa.h>
+
+double parsestr(const char* str) {
+    return [[NSString stringWithUTF8String:str] doubleValue];
+}
 
 int main(int argc, const char * argv[])
 {
-
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSColor* c = [[NSColor colorWithDeviceRed:parsestr(argv[1]) green:parsestr(argv[2]) blue:parsestr(argv[3]) alpha:1.0] colorUsingColorSpace:[NSColorSpace genericRGBColorSpace]];
         
+        printf("[NSColor colorWithCalibratedRed:%.3f green:%.3f blue:%.3f alpha:1.0]\n", [c redComponent], [c greenComponent], [c blueComponent]);        
     }
     return 0;
 }
