2534cff554636187aa6ecd6ef1011e43212f5a4b
Alex Gordon <anything@fileability.net>
2012-05-25 19:56:13 +0100

    Initial Commit

---
+++ colcal/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  colcal
+//
+//  Created by Alex Gordon on 25/05/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
